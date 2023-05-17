5029405a9217cd4232bdd01b488d6c9a4ef8aebf
Oliver Wolff <oliver.wolff@mrjaffesclass.com>
2023-05-16 22:25:07 -0700

    added a mouselistener need to fix it

--- src/View.java
+++ src/View.java
@@ -1,6 +1,9 @@
 import java.awt.*;
+import java.awt.event.MouseListener;
 import java.awt.geom.*;
 import javax.swing.*;
+
+import org.w3c.dom.events.MouseEvent;
 
 
 public class View extends JComponent{
@@ -9,27 +12,29 @@ public class View extends JComponent{
 
     public void init(int w, int h) {
        
-
         View v = new View(w,h);
         
-
+        
         JFrame f = new JFrame();
-        	
         JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
         f.add(panel);
+
         panel.setMinimumSize(new Dimension(w,h));
         panel.add(v);
         panel.setVisible(true);
+        panel.addMouseListener(new MouseListener() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                System.out.println("a");
+            }
+        });
 
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setMinimumSize(new Dimension(w, h));
-        
-        
         f.pack();
         f.setLocationRelativeTo(null);
         f.setVisible(true);
     }
-
 
     public View(int w, int h) {
         width = w;
